38318826a16727d308ff26834025cda3fd0fd39c
astro <45420135+astritm@users.noreply.github.com>
2021-08-31 22:13:48 +0200

    Create planets_webapp.py

---
+++ planets_webapp.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import json
+from typing import Counter
+import requests
+from datetime import datetime
+import time
+
+st.set_page_config(layout="wide")
+
+"""
+## Display $PLANET transactions
+If you try to get all transactions from Algoexplorer.io for a given wallet address, you will find a lot of transactions with 0 value.\n
+It's hard to identify the transactions with real amount of PLANETS.\n
+Therefore I have created this small web app to solve this issue.\n
+The webapp uses the Algoexplorer API with parameters like PLANETS asset-id: 27165954 and amount greater than 0.
+
+"""
+st.sidebar.write("")
+st.sidebar.title("About")
+st.sidebar.info(
+            """
+            The webapp is created by Astr0 to help the PW community and it is ad-free
+            and open-source. If you find the app useful you can support me here: 
+            Algorand: `3KBG44MVZSKKOUDW7QJ2QS2FYHFIHNTLT3Q7MTQ2CLG65ZHQ6RL6ENZ7GQ`  
+                     
+            You can find the source and contribute [here](https://github.com/astritm/planetwatch).  
+            
+            Special thanks to @feralfether, @inforkill, @Wagner and many more who have actively or passively
+            contributed to the project.
+            """
+            )
+
+
+Total_rx = 0
+Total_tx = 0
+Diff = 0
+Counter_tx=0
+Wallet_Address = st.text_input('Please enter a your wallet address without spaces and hit Enter')
+if len(Wallet_Address) == 58:
+
+    response = requests.get('https://algoexplorerapi.io/idx2/v2/transactions?address={}&asset-id=27165954&currency-greater-than=0'.format(Wallet_Address)).text
+    response_info = json.loads(response)
+
+    for transactions in response_info['transactions']:
+     amount = transactions['asset-transfer-transaction']['amount']
+     amount = amount / 1000000
+     amount = round (amount, 2)
+     timestamp = transactions['round-time']
+     your_date = datetime.fromtimestamp(timestamp)
+     Counter_tx = Counter_tx + 1
+     if transactions['asset-transfer-transaction']['receiver'] == Wallet_Address:
+        Total_rx = amount + Total_rx
+        st.write ("Date:" , your_date, "PLANETS: + " , amount, " txid: " , transactions['id'] )
+     if transactions['asset-transfer-transaction']['receiver'] != Wallet_Address:
+        Total_tx = amount + Total_tx
+        st.write ("Date:" , your_date , "PLANETS: - " , amount, " txid: " , transactions['id'] )
+    
+    st.write()
+    st.write("====================================")
+    Total_rx = round(Total_rx, 2)
+    st.write ("Total Planets Received: {}".format(Total_rx))
+    st.write()
+    Total_tx = round(Total_tx, 2)
+    st.write ("Total Planets Sent:     {}".format(Total_tx))
+    st.write()
+    Diff = Total_rx - Total_tx
+    Diff = round(Diff, 2)
+    st.write ("Diff: {}".format(Diff))
+    st.write()
+    st.write ("Total Transactions with PLANETS > 0: {}".format(Counter_tx))
+    st.write()
